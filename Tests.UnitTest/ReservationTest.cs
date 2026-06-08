6c58edb483f8b1a7048b7cfede281ec78e5db811
Simon0824 <szymon.szostek008@gmail.com>
2026-06-08 12:47:39 +0200

    Move repository root

---
+++ Tests.UnitTest/ReservationTest.cs
@@ -0,0 +1,47 @@
+using Xunit;
+using reservation_system.Models;
+using reservation_system.Services; 
+using reservation_system.Dtos;
+using reservation_system.Data;
+using Microsoft.EntityFrameworkCore;
+namespace Tests.UnitTest;
+
+public class ReservationTest
+{
+    private static readonly ReservationDto dto = new ReservationDto(new DateOnly(2026, 12, 24));
+    private ReservationContext CreateContext(string Db)
+    {
+        var options = new DbContextOptionsBuilder<ReservationContext>()
+            .UseInMemoryDatabase(Db)
+            .Options;
+            return new ReservationContext(options);
+    }
+
+    [Fact]
+    public async Task ReservationService_Should_ReturnNull_WhenDateIsTaken()
+    {
+        //Arrange
+        var context = CreateContext("taken_date");
+        var takenDate = new DateOnly(2026, 12, 24);
+        var _reservationService = new ReservationService(context);
+        context.Reservation.Add(new ReservationModel { ReservationDate = takenDate});
+        await context.SaveChangesAsync();
+        //Act
+        var result = await _reservationService.CreateNewReservation(dto);
+        //Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task ReservationService_Should_ReturnDto_WhenDateIsFree()
+    {
+        //Arrange
+        var context = CreateContext("free_date");
+        var _reservationService = new ReservationService(context);
+        //Act
+        var result = await _reservationService.CreateNewReservation(dto);
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(dto.ReservationDate, result.ReservationDate);
+    }
+}
---
+++ Tests.UnitTest/UserServiceTest.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Identity;
+using reservation_system.Dtos;
+using reservation_system.Models;
+using reservation_system.Services;
+using NSubstitute;
+using Xunit;
+using Microsoft.AspNetCore.Http;
+public class UserServiceTest
+{
+    private readonly UserManager<UserAppModel> _userMan;
+    private readonly UserService _userService;
+    private readonly SignInManager<UserAppModel> _signMan;
+    private readonly ITokenService _tokenService;
+    private readonly RegisterDto registerDto = new RegisterDto(Email: "example@email.com", Password: "P4$$w0rD", Name: "Name", LastName: "LastName");
+
+    public UserServiceTest()
+    {
+        var userStore = Substitute.For<IUserStore<UserAppModel>>();
+        _userMan = Substitute.For<UserManager<UserAppModel>>(
+            userStore, null, null, null, null, null, null, null, null);
+
+        var contextAccessor = Substitute.For<IHttpContextAccessor>();
+        var claimsFactory = Substitute.For<IUserClaimsPrincipalFactory<UserAppModel>>();
+
+        _signMan = Substitute.For<SignInManager<UserAppModel>>(
+            _userMan, contextAccessor, claimsFactory, null, null, null, null);
+
+        _tokenService = Substitute.For<ITokenService>();
+
+        _userService = new UserService(_userMan, _signMan, _tokenService);
+    }
+    [Fact]
+    public async Task UserRegister_Should_ReturnErrors_WhenUserExist()
+    {
+        //Arrange
+        UserAppModel existingUser = new()
+        {
+           Email = registerDto.Email,
+           PasswordHash = registerDto.Password, 
+           Name = registerDto.Name, 
+           LastName = registerDto.LastName
+        };
+
+        _userMan.FindByEmailAsync(existingUser.Email).Returns(existingUser);
+        _userMan.CreateAsync(Arg.Any<UserAppModel>(), Arg.Any<string>())
+             .Returns(IdentityResult.Failed(new IdentityError { Description = "User already exists" }));
+
+        //Act
+        var registerResult = await _userService.UserRegister(registerDto);
+        
+        //Assert
+        Assert.False(registerResult.Succeeded);
+        Assert.NotEmpty(registerResult.Errors);
+    }
+}
